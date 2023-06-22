0699b285259e1d46a1574c63d4bebcc31075416b
sertaoboy <raul.dev@hotmail.com>
2023-06-22 14:47:16 -0300

    parametros;att1

--- parametros.js
+++ parametros.js
@@ -46,10 +46,10 @@ console.log(multiplica(2, 2))
 console.log(multiplica(somaParametro(1, 1), somaParametro(2, 2))) // obs na linha 46: reparar que se utiliza uma funcao (somaParametro) para definir o parametro que sera utilizado na funcao "multiplica", e so ai sera apresentado no terminal a soma de 1+1 com a soma de 2+2, e ai a multiplicacao de 2*4!!!!!!!!!!!!!
 
 //observar o exemplo abaixo
-console.log(multiplica(soma(1, 1))) // o terminal retorna NaN -> porque a funcao "multiplica" espera receber DOIS parametros, e como nao foi passado o segundo param. ele atribui UNDEFINED -> logo, um numero * undefined nao e um numero -> NaN. Para evitar isso pode-se definir um VALOR PADRAO para algum parametro, observar exemplo abaixo
+console.log(multiplica(somaParametro(1, 1))) // o terminal retorna NaN -> porque a funcao "multiplica" espera receber DOIS parametros, e como nao foi passado o segundo param. ele atribui UNDEFINED -> logo, um numero * undefined nao e um numero -> NaN. Para evitar isso pode-se definir um VALOR PADRAO para algum parametro, observar exemplo abaixo
 function multiplicaPadrao(numeroI = 1, numeroII = 1) {
     return numeroI * numeroII;
 }
-console.log(multiplicaPadrao(soma(4, 5)))
+console.log(multiplicaPadrao(somaParametro(4, 5)))
 
 //Boa pratica em funcoes -> passar poucos argumentos para cada funcao. "Se uma funcao tiver varios argumentos, provalvemente voce pode quebrar ela em varias funcoes menores."
